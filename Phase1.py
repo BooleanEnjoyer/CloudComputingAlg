1673e2f464864f95ab8015e7f780d312c98b2cc2
dominik-badora <dominik.badora@comarch.com>
2024-12-03 17:13:44 +0100

    Constraint cleanup, refactor adding infeasible display

--- Phase1.py
+++ Phase1.py
@@ -5,8 +5,8 @@ def independent_optimization(tasks, task_subtasks, resources, costs, exec_times,
     wt, we = weights  # Weights for time and cost
     model = Model("IndependentOptimization")
 
+    # Constraint 7 constraint because it is binary
     b = model.addVars(tasks, task_subtasks, resources, vtype=GRB.BINARY, name="b")
-
 
     T_turnaround = model.addVar(vtype=GRB.CONTINUOUS, name="T_turnaround")
 
@@ -15,21 +15,23 @@ def independent_optimization(tasks, task_subtasks, resources, costs, exec_times,
         GRB.MINIMIZE
     )
 
-    # Constraints:
     for i in tasks:
-        for k in task_subtasks[i]:
-            model.addConstr(quicksum(b[i, k, j] for j in resources) == 1, name=f"Subtask_{i}_{k}_Assignment")
-
-
-        model.addConstr(quicksum(exec_times[i, k] * b[i, k, j] for k in task_subtasks for j in resources) <= deadlines[i],
+        # Constraint 8
+        model.addConstr(T_turnaround <= deadlines[i],
             name=f"Deadline_Task_{i}"
         )
 
+        # Constraint 9
         model.addConstr(
             quicksum(costs[i, k] * b[i, k, j] for k in task_subtasks for j in resources) <= budgets[i],
             name=f"Budget_Task_{i}"
         )
 
+        # Constraint 10
+        for k in task_subtasks[i]:
+            model.addConstr(quicksum(b[i, k, j] for j in resources) == 1, name=f"Subtask_{i}_{k}_Assignment")
+
+    # Constraint 11
     for j in resources:
         for i in tasks:
             model.addConstr(
@@ -37,17 +39,15 @@ def independent_optimization(tasks, task_subtasks, resources, costs, exec_times,
                 name=f"Resource_{j}_Task_{i}_Capacity"
             )
 
-    # Turnaround time constraint (execution + communication times)
-    # TODO dodać tutaj może bij???????
+    # Constraint 12 (simplified max)
     model.addConstr(
-        T_turnaround >= quicksum(tl) + quicksum(exec_times[i, k] for i in tasks for k in task_subtasks),
+        T_turnaround >= quicksum(tl) + quicksum(exec_times[i, k] * b[i, k, j] for i in tasks for k in task_subtasks for j in resources),
         name="TurnaroundTime"
     )
 
     model.optimize()
 
-
-    # Display
+    # Display section
     allocation_info = []
     allocation_matrix = []
 
@@ -66,3 +66,11 @@ def independent_optimization(tasks, task_subtasks, resources, costs, exec_times,
             allocation_matrix.append(task_row)
 
         return "\n".join(allocation_info), allocation_matrix, model.ObjVal
+    elif model.Status == GRB.INFEASIBLE:
+        print("Model is infeasible. Relaxing constraints to diagnose issues.")
+        model.computeIIS()
+        model.write("model.ilp")
+        return None, None
+    else:
+        print("Optimization failed")
+        return None, None
