6c9a22653f1b995e0283da0d740dfb769e79f2b8
Adam Winnik <adam.winnik120@gmail.com>
2024-12-03 19:30:57 +0100

    changed shit

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from Phase1 import independent_optimization
 
-tasks = [1, 2, 3]
+tasks = [0, 1, 2]
 task_subtasks = {
-    1: [1],
-    2: [1, 2],
-    3: [1, 2, 3],
+    0: [0, 1],
+    1: [0, 1, 2],
+    2: [0, 1, 2, 3],
 }
 
-resources = [1, 2, 3, 4, 5]
+resources = [0, 1, 2, 3, 4]
 
 exec_times = {
-    (1, 1): 6, (1, 2): 5, (1, 3): 4,
-    (2, 1): 5, (2, 2): 4.2, (2, 3): 3.6,
-    (3, 1): 4, (3, 2): 3.5, (3, 3): 3.2
+    (0, 0): 6, (0, 1): 5, (0, 2): 4, (0, 3): 3.5, (0, 4): 3,
+    (1, 0): 5, (1, 1): 4.2, (1, 2): 3.6, (1, 3): 3, (1, 4): 2.8,
+    (2, 0): 4, (2, 1): 3.5, (2, 2): 3.2, (2, 3): 2.8, (2, 4): 2.4
 }
-costs = {key: key[0] * exec_times[key] for key in exec_times}
 
-deadlines = {1: 50, 2: 70, 3: 100}
-budgets = {1: 50, 2: 60, 3: 70}
-tl = [1, 2, 3]
+p = [1, 1.2, 1.5, 1.8, 2]
+
+costs = {key: p[key[0]] * exec_times[key] for key in exec_times}
+
+deadlines = {0: 50000, 1: 70000, 2: 10000}
+budgets = {0: 50000, 1: 60000, 2: 700000}
+tl = [1, 1, 1]
 weights = (0.5, 0.5)
 
 allocation_info, allocation_matrix, obj_value = independent_optimization(tasks, task_subtasks, resources, costs, exec_times, deadlines, budgets, weights, tl)
